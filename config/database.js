51d790e22f3c76097d6375a262675173ef329340
mdylanr <dylancogan20@gmail.com>
2023-12-07 04:50:02 +0700

    setup env

--- config/database.js
+++ config/database.js
@@ -1,8 +1,21 @@
 import { Sequelize } from 'sequelize';
+import {dotenv} from "dotenv";
 
-const db = new Sequelize('newsportal','root','',{
-    host: "localhost",
-    dialect: "mysql",
-})
+dotenv.config();
+
+
+const {DB_NAME, DB_PASSWORD, DB_USER, DB_URI} = process.env
+const db = new Sequelize(DB_URI,{
+    define:{
+        timestamps: false
+    }
+});
+
+
+//local
+// const db = new Sequelize('DB_NAME','DB_USER','DB_PASSWORD',{
+//     host: "DB_HOST",
+//     dialect: "DB_DIALECT",
+// })
 
 export default db
